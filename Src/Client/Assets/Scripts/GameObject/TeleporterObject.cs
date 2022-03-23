eb55c340d1172eab2678737892db6e0da7859127
WangRuikang <ruikangwang@foxmail.com>
2022-03-23 22:03:47 +0800

    1、上传美术资源 2、制作传送点（未完成）

---
+++ Src/Client/Assets/Scripts/GameObject/TeleporterObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleporterObject : MonoBehaviour {
+
+    public int id;
+    Mesh mesh = null;
+	void Start () {
+        this.mesh = this.GetComponent<MeshFilter>().sharedMesh;
+	}
+	
+	void Update () {
+		
+	}
+    //仅在编辑器模式有效
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        if (this.mesh != null)
+        {
+            //Gizmos.DrawWireMesh(this.mesh, this.transform.position + Vector3.up * this.transform.localScale.y * 0.5f, this.transform.rotation, this.transform.localScale);
+            Gizmos.DrawWireMesh(this.mesh, this.transform.position, this.transform.rotation, this.transform.localScale);
+        }
+        UnityEditor.Handles.color = Color.red;
+        UnityEditor.Handles.ArrowHandleCap(0, this.transform.position, this.transform.rotation, 1f, EventType.Repaint);
+    }
+#endif
+}
