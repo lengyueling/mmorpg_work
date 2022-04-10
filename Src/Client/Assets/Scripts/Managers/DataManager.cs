4145cd6bf5d210a805009ef4bf34a613d08e0c39
lengyueling <1030952490@qq.com>
2022-04-10 23:37:46 +0800

    编写任务系统 1.初步完成任务系统UI 2.完成任务系统策划 3.完成任务系统配置表 4.完成任务系统protobuf协议

--- Src/Client/Assets/Scripts/Managers/DataManager.cs
+++ Src/Client/Assets/Scripts/Managers/DataManager.cs
@@ -20,6 +20,7 @@ public class DataManager : Singleton<DataManager>
     public Dictionary<int, ShopDefine> Shops = null;
     public Dictionary<int, Dictionary<int, ShopItemDefine>> ShopItems = null;
     public Dictionary<int, EquipDefine> Equips = null;
+    public Dictionary<int, QuestDefine> Quests = null;
 
 
     public DataManager()
@@ -99,6 +100,9 @@ public class DataManager : Singleton<DataManager>
 
         json = File.ReadAllText(this.DataPath + "EquipDefine.txt");
         this.Equips = JsonConvert.DeserializeObject<Dictionary<int, EquipDefine>>(json);
+
+        json = File.ReadAllText(this.DataPath + "QuestDefine.txt");
+        this.Quests = JsonConvert.DeserializeObject<Dictionary<int, QuestDefine>>(json);
 
         yield return null;
     }
